84c6fefe3944b59c5dcfdf761cc6890d8d6bb2df
_LandenBarkerGames <93301002+LandenTy@users.noreply.github.com>
2024-02-07 15:33:19 -0600

    Add files via upload

---
+++ Game Window/User/Images/image.bat
@@ -0,0 +1,16 @@
+@echo off
+color a
+echo.
+type d0.txt
+echo.
+
+timeout /nobreak /t 1 /nobreak > nul
+cls
+type d1.txt
+echo.
+
+timeout /nobreak /t 1 /nobreak > nul
+cls
+type d2.txt
+echo.
+pause
